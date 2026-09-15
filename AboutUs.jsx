fb4273500cdd41692f123c79ec6c54a536960b58
Nidhi Haridas <nidhiharidas12@gmail.com>
2026-09-16 02:31:50 +0530

    Add AboutUs component with nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your one-stop destination for beautiful
+        and healthy houseplants.
+      </p>
+
+      <p>
+        We provide a wide variety of indoor plants that can brighten your
+        home, improve your surroundings, and bring nature closer to you.
+      </p>
+
+      <p>
+        Our goal is to make plant shopping simple and convenient by offering
+        different types of plants with useful information, affordable prices,
+        and an easy-to-use shopping experience.
+      </p>
+
+      <h2>Our Mission</h2>
+
+      <p>
+        Our mission is to encourage people to bring more greenery into their
+        homes and create a healthier and more pleasant living environment.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
